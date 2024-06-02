73ce2e54cf6026d6762909bba1511081ad25d3a3
Pratham271 <chauhanpratham22@gmail.com>
2024-06-02 16:54:51 +0530

    added the signup route

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,8 +1,7 @@
 import express from "express";
 import cors from "cors";
-import dotenv from "dotenv"
 import userRouter from "./routes/user";
-dotenv.config()
+
 const app = express()
 
 app.use(express.json())
---
+++ backend/src/middlewares/index.ts
@@ -0,0 +1,39 @@
+import jwt from "jsonwebtoken"
+import bcrypt from "bcrypt"
+import { NextFunction, Request, Response } from "express"
+import { signupSchema } from "../zod"
+import { User } from "../db"
+
+
+interface signupInputs {
+    firstName: string
+    lastName: string
+    email:string
+    password: string
+}
+
+export const signupMiddleWare = async(req:Request,res:Response,next:NextFunction) => {
+    const data:signupInputs = req.body
+
+    try {
+        const parsedData= signupSchema.safeParse(data)
+        if(!parsedData.success){
+            return res.status(411).json({
+                message: "wrong inputs"
+            })
+        }
+        const user = await User.findOne({email: data.email})
+
+        if(user){
+            res.status(403).json({
+                message: "Email already exists"
+            })
+        }
+        next();
+    } catch (error) {
+        console.log(error)
+        res.status(500).json({
+            errorMessage: "Something went wrong"
+        })
+    }
+}
