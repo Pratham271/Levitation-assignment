2fc9c25806e54e51ff09e702303b82b0dd133340
Pratham271 <chauhanpratham22@gmail.com>
2024-06-02 16:24:24 +0530

    added the database support

---
+++ backend/db/index.ts
@@ -0,0 +1,69 @@
+import mongoose from "mongoose";
+
+mongoose.connect(process.env.MONGOOSE_CONNECTION_STRING!)
+.then(()=> console.log("connected to mongoose"))
+.catch((e) => console.log(e))
+
+const userSchema = new mongoose.Schema({
+    firstName: {
+        type: String,
+        required: true,
+        trim: true,
+        maxLength: 30
+    },
+    lastName: {
+        type: String,
+        required: true,
+        trim: true,
+        maxLength: 30
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true,
+    }
+    ,
+    password: {
+        type: String,
+        required: true,
+        minLength: 6,
+        maxLength: 16
+    }
+})
+
+const cartSchema = new mongoose.Schema({
+    userId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'user',
+        required: true
+    },
+    productName : {
+        type: String,
+        required: true
+    },
+    quantity: {
+        type: Number,
+        required: true,
+        default: 1
+    },
+    price: {
+        type: Number,
+        required: true
+    },
+    validity: {
+        type: Date,
+        default: function() {
+            const currentDate = new Date();
+            const nextYearDate = new Date(currentDate.setFullYear(currentDate.getFullYear() + 1));
+            return nextYearDate;
+        }
+    },
+    
+
+},{ timestamps: true })
+
+const User = mongoose.model("user", userSchema)
+const Cart = mongoose.model("cart", cartSchema)
+
+module.exports = {User, Cart}
